7a9a6f2b277f85c97a2a9e93e25c4c53a61aba1f
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-10 21:11:41 +0700

    pf-java selection sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -2,18 +2,18 @@ public class SelectionSort {
     public static void selectionSort(int[] list) {
         for (int i = 0; i < list.length - 1; i++) {
             /* Find the minimum in the list[i..list.length-1] */
-            int elementMax = list[i];
+            int elementMin = list[i];
             int elemnetIndex = i;
             for (int j = i + 1; j < list.length; j++) {
-                if (elementMax < list[j]) {
-                    elementMax = list[j];
+                if (elementMin < list[j]) {
+                    elementMin = list[j];
                     elemnetIndex = j;
                 }
             }
             /* Swap list[i] with list[currentMinIndex] if necessary */
             if (elemnetIndex != i) {
                 list[elemnetIndex] = list[i];
-                list[i] = elementMax;
+                list[i] = elementMin;
             }
         }
     }
